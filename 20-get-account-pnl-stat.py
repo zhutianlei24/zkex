c8284af3671c4f3b01d6fd5f5936404a22352f23
zhutianlei24 <zhutianlei2b@gmail.com>
2023-09-03 07:05:52 +0200

    chanegs

---
+++ 20-get-account-pnl-stat.py
@@ -0,0 +1,45 @@
+import requests
+import pickledb
+
+jwt = pickledb.load('jwt.db', False).get("jwt")
+
+# So here we need to attach the jwt token which we obtained from step 5 to the request headers,
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
+    "Access-Token": jwt  
+}
+
+response = requests.get(
+    "https://aws-test-1.zkex.com/api-v1/api/accounts/pnlStat",
+    headers = headers
+)
+
+print("Sending request to:")
+
+print(">>>>>>>>>>>>>>>>>>>")
+
+print(response.url)
+
+print(">>>>>>>>>>>>>>>>>>>")
+
+print()
+
+print("Getting response:")
+
+print("<<<<<<<<<<<<<<<<<<<")
+
+print(response.text)
+
+print("<<<<<<<<<<<<<<<<<<<")
+
+print()
+
+print("See more docs here:")
+
+print("===================")
+
+print("https://github.com/ZKEX/orderbook-apis#getaccountpnlstats")
+
+print("===================")
+
+
